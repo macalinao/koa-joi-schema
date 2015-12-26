06b4cd75c18ebd3786a7644903c0e7bff74de971
Ian Macalinao <me@ian.pw>
2015-12-25 23:41:24 -0500

    feat: Implement Joi validation

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const co = require('co')
 const get = require('get-value')
+const promisify = require('es6-promisify')
+const Joi = require('joi')
 
-module.exports = path => schema => function *(ctx, next) {
+module.exports = path => schema => (ctx, next) => {
   const value = get(ctx, path)
+  return promisify(Joi.validate)(value, schema)
+    .then((result) => next())
+    .catch(handleError(ctx))
 }
+
+const handleError = ctx => err => {
+  ctx.joiError = err
+}
+
+module.exports.Joi = Joi
--- test.js
+++ test.js
@@ -1,10 +1,42 @@
+const co = require('co')
 const expect = require('chai').expect
 const validate = require('.')
+const Joi = validate.Joi
+
+const next = () => Promise.resolve(true)
 
 describe('koa-joi-schema', () => {
 
-  it('should check schema on body', () => {
-    const validateBody = validate('body')
+  it('should validate input matching the schema', () => {
+    const ctx = {
+      body: {
+        name: 'Test'
+      }
+    }
+    const validator = validate('body')(Joi.object().keys({
+      name: Joi.string()
+    }))
+    return co(function *() {
+      yield validator(ctx, next)
+      expect(ctx.joiError).to.be.undefined
+    })
+  })
+
+  it('should error on input not matching the schema', () => {
+    const ctx = {
+      body: {
+        password: 'boracay waling waling'
+      }
+    }
+    const validator = validate('body')(Joi.object().keys({
+      name: Joi.string().required()
+    }))
+    return co(function *() {
+      yield validator(ctx, next)
+      expect(ctx.joiError.name).to.equal('ValidationError')
+      expect(ctx.joiError.details.length).to.equal(1)
+      expect(ctx.joiError.details[0].path).to.equal('name')
+    })
   })
 
 })
